11888e876f594cc0b26f10c05df81275a063b35f
widward <61576370+Kuroko-zzz@users.noreply.github.com>
2022-02-28 13:57:34 +0000

    Add files via upload

---
+++ dwm-6.3/patch/include.c
@@ -0,0 +1,19 @@
+/* Bar functionality */
+#include "bar_indicators.c"
+#include "bar_tagicons.c"
+
+#include "bar_alpha.c"
+#include "bar_ltsymbol.c"
+#include "bar_tags.c"
+#include "bar_wintitle.c"
+#include "bar_awesomebar.c"
+#include "bar_wintitleactions.c"
+
+/* Other patches */
+#include "fullscreen.c"
+#include "vanitygaps.c"
+/* Layouts */
+#include "layout_facts.c"
+#include "layout_monocle.c"
+#include "layout_tile.c"
+
---
+++ dwm-6.3/patch/include.h
@@ -0,0 +1,18 @@
+/* Bar functionality */
+#include "bar_indicators.h"
+#include "bar_tagicons.h"
+
+#include "bar_alpha.h"
+#include "bar_ltsymbol.h"
+#include "bar_tags.h"
+#include "bar_wintitle.h"
+#include "bar_awesomebar.h"
+#include "bar_wintitleactions.h"
+
+/* Other patches */
+#include "fullscreen.h"
+#include "vanitygaps.h"
+/* Layouts */
+#include "layout_monocle.h"
+#include "layout_tile.h"
+
