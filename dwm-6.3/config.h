b9a9239c0fbaf906da38e86cc7e4bd8c6be085dd
widward <61576370+Kuroko-zzz@users.noreply.github.com>
2022-03-14 01:38:17 +0000

    Add files via upload

--- dwm-6.3/config.h
+++ dwm-6.3/config.h
@@ -123,8 +123,8 @@ static Key keys[] = {
 	{ MODKEY,                       XK_period, focusmon,       {.i = +1 } },
 	{ MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
 	{ MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
-	{ MODKEY,                       XK_s,      show,           {0} },
-	{ MODKEY,                       XK_h,      hide,           {0} },
+	{ MODKEY,                       XK_u,      show,           {0} },
+	{ MODKEY,                       XK_o,      hide,           {0} },
 	TAGKEYS(                        XK_1,                      0)
 	TAGKEYS(                        XK_2,                      1)
 	TAGKEYS(                        XK_3,                      2)
