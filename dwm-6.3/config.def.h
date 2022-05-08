ef5fe44e514255b487ae22323d0e91f871da563a
widward <61576370+Widward1@users.noreply.github.com>
2022-05-08 06:48:56 -0700

    Add files via upload

--- dwm-6.3/config.def.h
+++ dwm-6.3/config.def.h
@@ -82,7 +82,7 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_prp, "-sb", col_bg, "-sf", col_grn, NULL };
 static const char *termcmd[]  = { "st", NULL };
 static const char *webcmd[] = { "qutebrowser", NULL};
 static const char *disccmd[]  = { "discord","--ignore-gpu-blocklist","--disable-features=UseOzonePlatform","--enable-features=VaapiVideoDecoder","--use-gl=desktop","--enable-gpu-rasterization","--enable-zero-copy", NULL };
