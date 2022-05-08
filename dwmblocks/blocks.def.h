d6e687eb3744e2a2eab351cdcb1033ccb6650a8e
widward <61576370+Widward1@users.noreply.github.com>
2022-05-08 06:19:19 -0700

    Add files via upload

--- dwmblocks/blocks.def.h
+++ dwmblocks/blocks.def.h
@@ -2,7 +2,10 @@
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
 
-	{"", "date '+%b %d (%a) %I:%M%p'",					5,		0},
+//	{"", "date '+%b %d (%a) %I:%M%p'",					5,		0},
+	{"|  ", "date '+%I:%M%p'",					    5,		0},
+    {" ", "~/widwardDWM/dwmblocks/scripts/volume",  5,      0},
+    {" ", "~/widwardDWM/dwmblocks/scripts/wifi",  5,      0},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
