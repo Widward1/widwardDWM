cfcad5b5cfe09d7b42115ddb4a433f43b45456e9
widward <61576370+Widward1@users.noreply.github.com>
2022-05-09 08:07:03 -0700

    Update config.def.h

--- dmenu-5.1/config.def.h
+++ dmenu-5.1/config.def.h
@@ -26,4 +26,4 @@ static unsigned int min_lineheight = 8;
 static const char worddelimiters[] = " ";
 
 /* Size of the window border */
-static const unsigned int border_width = 3;
+static const unsigned int border_width = 1;
