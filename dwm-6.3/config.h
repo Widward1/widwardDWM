515ee4154098fc428a95c92c51259653336e00fa
widward <61576370+Widward1@users.noreply.github.com>
2022-07-28 11:00:43 +0000

    Add files via upload

--- dwm-6.3/config.h
+++ dwm-6.3/config.h
@@ -1,5 +1,5 @@
 /* See LICENSE file for copyright and license details. */
-
+#include <X11/XF86keysym.h>
 /* appearance */
 static const unsigned int borderpx  = 3;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
@@ -84,16 +84,27 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_prp, "-sb", col_grn, "-sf", col_bg, NULL };
 static const char *termcmd[]  = { "st", NULL };
-static const char *webcmd[] = { "qutebrowser", NULL};
+static const char *webcmd[] = { "firefox-bin", NULL};
 static const char *disccmd[]  = { "discord","--ignore-gpu-blocklist","--disable-features=UseOzonePlatform","--enable-features=VaapiVideoDecoder","--use-gl=desktop","--enable-gpu-rasterization","--enable-zero-copy", NULL };
-
+static const char *upvol[]   = { "/usr/bin/pactl", "set-sink-volume", "0", "+5%",     NULL };
+static const char *downvol[] = { "/usr/bin/pactl", "set-sink-volume", "0", "-5%",     NULL };
+static const char *mutevol[] = { "/usr/bin/pactl", "set-sink-mute",   "0", "toggle",  NULL };
+static const char *upbright[] = {"/usr/bin/xbacklight", "-dec", "10", NULL};
+static const char *downbright[] = {"/usr/bin/xbacklight", "-inc", "10", NULL};
+static const char *screenshot[] = {"/usr/bin/screenshotmaim", "select", NULL};
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
 	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
 	{ MODKEY|ShiftMask,             XK_d,      spawn,          {.v = disccmd } },
-	{ MODKEY|ShiftMask,             XK_c,      spawn,          {.v = webcmd } },
+	{ MODKEY|ShiftMask,             XK_c,      spawn,          {.v = webcmd } },		
+	{ MODKEY|ShiftMask,             XK_s,      spawn,          {.v = screenshot } },		
+	{ 0,                       XF86XK_AudioLowerVolume, spawn, {.v = downvol } },
+	{ 0,                       XF86XK_AudioMute, spawn, {.v = mutevol } },
+        { 0,                       XF86XK_AudioRaiseVolume, spawn, {.v = upvol   } },
+        { 0,                       XF86XK_MonBrightnessDown, spawn, {.v = upbright   } },
+        { 0,                       XF86XK_MonBrightnessUp, spawn, {.v = downbright   } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
@@ -122,7 +133,8 @@ static Key keys[] = {
 	{ MODKEY,                       XK_q,      killclient,     {0} },
 	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
 	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },
-	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
+	{ MODKEY,                       XK_o,      setlayout,      {.v = &layouts[2]} },
+	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[3]} },
 	{ MODKEY,                       XK_space,  setlayout,      {0} },
 	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
 	{ MODKEY|ShiftMask,             XK_f,      togglefullscr,  {0} },
--- dwmblocks/blocks.h
+++ dwmblocks/blocks.h
@@ -3,11 +3,12 @@ static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
 
 //	{"", "date '+%b %d (%a) %I:%M%p'",					5,		0},
-	{"|  ", "date '+%I:%M%p'",					    5,		0},
-    {" ", "~/widwardDWM/dwmblocks/scripts/volume",  5,      0},
-    {" ", "~/widwardDWM/dwmblocks/scripts/wifi ",  5,      0},
+    {" ", "date '+%I:%M%p'",			      5,      0},
+//  {"", "~/widwardDWM/dwmblocks/scripts/volume",     5,      0},
+    {"batt ", "~/widwardDWM/dwmblocks/scripts/batt",  5,      0},
+    {"wifi ", "~/widwardDWM/dwmblocks/scripts/wifi",  5,      0},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
-static char delim[] = " | ";
+static char delim[] = " * ";
 static unsigned int delimLen = 5;
